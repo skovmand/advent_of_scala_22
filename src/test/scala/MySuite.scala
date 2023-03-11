e21b5a59ac2436e45ef8c4aaf8834d98c8c93b4a
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2023-03-11 15:26:56 +0100

    Have no idea what I'm doing

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -7,3 +7,20 @@ class MySuite extends munit.FunSuite {
     assertEquals(obtained, expected)
   }
 }
+
+val day01Example =
+  """1000
+    |2000
+    |3000
+    |
+    |4000
+    |
+    |5000
+    |6000
+    |
+    |7000
+    |8000
+    |9000
+    |
+    |10000
+    |""".stripMargin
