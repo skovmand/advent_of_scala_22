992ffccec9f04b57b626f0175bf73fae570a0737
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2023-03-13 21:08:31 +0100

    Add part 2

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -10,16 +10,53 @@ object Day01 {
         println(s"Part 1: Elf with max calories is $solution")
       }
       case Failure(exception) => {
-        throw new RuntimeException("Could not solve puzzle", exception)
+        throw new RuntimeException("Could not solve day 1 part 1", exception)
+      }
+    }
+
+    solvePart2(input) match {
+      case Success(solution) => {
+        println(s"Part 2: Sum of calories for top three Elves is $solution")
+      }
+      case Failure(exception) => {
+        throw new RuntimeException("Could not solve day 1 part 2", exception)
       }
     }
   }
+
+  // Part 1
 
   private def solvePart1(input: String): Try[Int] = {
     Try(input)
       .flatMap(parseInput)
       .flatMap(findElfWithMaxCalories)
   }
+
+  private def findElfWithMaxCalories(elfItemCalories: Vector[Vector[Int]]): Try[Int] = {
+    Try {
+      elfItemCalories
+        .map(_.sum)
+        .max
+    }
+  }
+
+  // Part 2
+
+  private def solvePart2(input: String): Try[Int] = {
+    Try(input)
+      .flatMap(parseInput)
+      .map(findTop3Elves)
+  }
+
+  private def findTop3Elves(elfItemCalories: Vector[Vector[Int]]): Int = {
+    elfItemCalories
+      .map(_.sum)
+      .sortWith(_ > _)
+      .take(3)
+      .sum
+  }
+
+  // Parsing
 
   private def parseInput(input: String): Try[Vector[Vector[Int]]] = {
     Try {
@@ -31,14 +68,6 @@ object Day01 {
             .map(_.toInt)
             .toVector
         )
-    }
-  }
-
-  private def findElfWithMaxCalories(elfItemCalories: Vector[Vector[Int]]): Try[Int] = {
-    Try {
-      elfItemCalories
-        .map(_.sum)
-        .max
     }
   }
 }
